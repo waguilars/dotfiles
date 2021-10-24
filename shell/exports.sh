87bfd50fee93bdbe3a80a19bad6aa719b8374e90
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2021-10-24 13:56:47 -0500

    :sparkles: add new theme

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,7 +2,7 @@
 # Codely theme config
 # ------------------------------------------------------------------------------
 export CODELY_THEME_MINIMAL=false
-export CODELY_THEME_MODE="dark"
+export CODELY_THEME_MODE=" " #dark default
 export CODELY_THEME_PROMPT_IN_NEW_LINE=false
 export CODELY_THEME_PWD_MODE="short" # full, short, home_relative
 
@@ -24,7 +24,7 @@ export PYSPARK_PYTHON=/usr/bin/python3
 if [ "$CODELY_THEME_MODE" = "dark" ]; then
   fzf_colors="pointer:#729ECF,bg+:#414868,fg:#7dcfff,fg+:#7aa2f7,hl:#73daca,info:#565f89,header:#f7768e"
 else
-  fzf_colors="pointer:#db0f35,bg+:#d6d6d6,fg:#808080,fg+:#363636,hl:#8ec07c,info:#928374,header:#fffee3"
+  fzf_colors="pointer:#a277ff,bg+:#15141b,fg:#edecee,fg+:#a277ff,hl:#61ffca,info:#a277ff,header:#ff6767"
 fi
 
 export FZF_DEFAULT_OPTS="--color=$fzf_colors --reverse"
--- shell/functions.sh
+++ shell/functions.sh
@@ -1,5 +1,5 @@
 function cdd() {
-  cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
+  cd "$(ls -d --no-icons -- */ | fzf)" || echo "Invalid directory"
 }
 
 function j() {
@@ -50,5 +50,37 @@ function ggp() {
   else
     [[ "$#" == 0 ]] && local b="$(git_current_branch)"
     git push origin "${b:=$1}"
+  fi
+}
+
+# take functions
+
+# mkcd is equivalent to takedir
+function takedir() {
+  mkdir -p $@ && cd ${@:$#}
+}
+
+function takeurl() {
+  local data thedir
+  data="$(mktemp)"
+  curl -L "$1" > "$data"
+  tar xf "$data"
+  thedir="$(tar tf "$data" | head -1)"
+  rm "$data"
+  cd "$thedir"
+}
+
+function takegit() {
+  git clone "$1"
+  cd "$(basename ${1%%.git})"
+}
+
+function take() {
+  if [[ $1 =~ ^(https?|ftp).*\.tar\.(gz|bz2|xz)$ ]]; then
+    takeurl "$1"
+  elif [[ $1 =~ ^([A-Za-z0-9]\+@|https?|git|ssh|ftps?|rsync).*\.git/?$ ]]; then
+    takegit "$1"
+  else
+    takedir "$@"
   fi
 }
