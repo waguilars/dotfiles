48a186cb272734a45cadfefb0f2b5c7eaedec92e
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2022-01-29 15:56:49 -0500

    add init script nvm

--- shell/init.sh
+++ shell/init.sh
@@ -3,3 +3,8 @@
 source "$DOTFILES_PATH/shell/aliases.sh"
 source "$DOTFILES_PATH/shell/exports.sh"
 source "$DOTFILES_PATH/shell/functions.sh"
+
+# Source init scripts
+for file in "$DOTFILES_PATH/shell/init.scripts"/*; do
+  source "$file"
+done
